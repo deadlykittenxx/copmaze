ce98097d5111dec5aa1e571f8da69005b66621b0
Jihyun Park <audrey123012@gmail.com>
2023-09-21 21:40:26 +0200

    Added how to play GUI

--- CopMaze/src/CopMaze.java
+++ CopMaze/src/CopMaze.java
@@ -14,8 +14,11 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class CopMaze extends Application {
@@ -35,9 +38,15 @@ public class CopMaze extends Application {
 	public Scene characterScene;
 	public Scene levelScene;
 	public Scene mazeScene;
+	public Scene ruleScene;
 	public EventHandler<ActionEvent> btnStartListener;
 	public EventHandler<ActionEvent> btnCharacterListener;
 	public EventHandler<ActionEvent> btnLevelListener;
+	public EventHandler<ActionEvent> btnNextListener;
+	public EventHandler<ActionEvent> btnHowtoPlayListener;
+	public Text Ruletxt;
+	public String[] RuleContent = new String[5];
+	public int count = 0;
 	
 	public static void main(String[] args){
 		launch(args);
@@ -62,13 +71,17 @@ public class CopMaze extends Application {
 		VBox mazeRoot = new VBox();
 		mazeScene = new Scene(mazeRoot);
 		
-		initListener(primaryStage);
+		VBox ruleRoot = new VBox();
+		ruleScene = new Scene(ruleRoot);
+		ruleRoot.setAlignment(Pos.CENTER);
 		
+		initListener(primaryStage);
 		
 		initGUI(root);
 		characterGUI(characterRoot);
 		levelGUI(levelRoot);
 		mazeGUI(mazeRoot);
+		ruleGUI(ruleRoot);
 		
 		primaryStage.setTitle("Cop Maze"); 
 		primaryStage.setWidth(WIDTH);
@@ -104,11 +117,23 @@ public class CopMaze extends Application {
 				 */
 				Button btn = (Button) event.getSource();
 				Character player = new Character(btn.getText());
-
 				stage.setScene(levelScene);
-				
 			}
-			
+		};
+		
+		btnHowtoPlayListener = new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				stage.setScene(ruleScene);
+			}
+		};
+		
+		btnNextListener = new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				count++;
+				Ruletxt.setText(RuleContent[count]);
+			}
 		};
 		
 	}
@@ -180,9 +205,8 @@ public class CopMaze extends Application {
 		Button btnHowtoPlay = new Button("How to Play");
 		Button btnStart = new Button("Start");
 		
+		btnHowtoPlay.setOnAction(btnHowtoPlayListener);
 		btnStart.setOnAction(btnStartListener);
-		
-		
 		
 		root.getChildren().addAll(label, btnHowtoPlay, btnStart);
 		
@@ -253,6 +277,38 @@ public class CopMaze extends Application {
             }
         }
     }
+
+
+	public void ruleGUI (Pane root) {
+		Label label = new Label("How to play!");
+		label.setPadding(new Insets(20, 50, 50, 50));
+		
+		Rectangle rectangle = new Rectangle(400, 300);
+		rectangle.setArcHeight(30);
+		rectangle.setArcWidth(30);
+		rectangle.setStroke(Color.TRANSPARENT);
+		rectangle.setStyle("-fx-fill: red; -fx-opacity: 0.5;");
+		
+		RuleContent[0] = "1. Move around the maze and collect all the gems.";
+		RuleContent[1] = "2. You should avoid policmen while moving around.";
+		RuleContent[2] = "3. When the key appears, go get it.";
+		RuleContent[3] = "4. Drag the key and drop to the door to unlock it.";
+		RuleContent[4] = "5. Escape through the opened door.";
+		
+		Ruletxt = new Text();
+		Ruletxt.setText(RuleContent[0]);
+		
+		StackPane stack = new StackPane();
+		stack.getChildren().addAll(rectangle, Ruletxt);
+		
+		Button btnNext = new Button("Next");
+		btnNext.setAlignment(Pos.CENTER_RIGHT);
+		
+		btnNext.setOnAction(btnNextListener);
+		
+		root.getChildren().addAll(label, stack, btnNext);
+		
+	}
 	
 	
 }
