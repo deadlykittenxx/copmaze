23fe1934fa62c7d2e5442296084a1ad490a90538
deadlykittenxx <96yean@gmail.com>
2023-09-21 14:53:16 +0200

    Create layout

--- CopMaze/src/CopMaze.java
+++ CopMaze/src/CopMaze.java
@@ -1,9 +1,27 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class CopMaze {
+public class CopMaze extends Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args){
+		launch(args);
 	}
-
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+			
+		FlowPane root = new FlowPane();
+		
+				
+		Scene scene = new Scene(root, 300, 300);
+		
+		primaryStage.setTitle("Layout Window!");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	
 }
