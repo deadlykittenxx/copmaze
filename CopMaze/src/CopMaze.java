995feb47a3bde1348d09b2c693ab66b69f3876d4
deadlykittenxx <96yean@gmail.com>
2023-09-21 15:01:27 +0200

    First Scene

--- CopMaze/src/CopMaze.java
+++ CopMaze/src/CopMaze.java
@@ -1,7 +1,12 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class CopMaze extends Application {
@@ -12,15 +17,32 @@ public class CopMaze extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-			
-		FlowPane root = new FlowPane();
 		
-				
-		Scene scene = new Scene(root, 300, 300);
+	
+		VBox root = new VBox();
+		Scene scene = new Scene(root, 700, 500);
+		root.setAlignment(Pos.CENTER);
 		
-		primaryStage.setTitle("Layout Window!");
+		initGUI(root);
+		primaryStage.setTitle("Cop Maze"); 
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	
+	
+	/*
+	 * 	Initalize the first Scene
+	 */
+	public void initGUI(Pane root) {
+		Label label = new Label("Cop Maze");
+		label.setPadding(new Insets(20, 50, 50, 50));
+		
+		Button btnHowtoPlay = new Button("How to Play");
+		Button btnStart = new Button("Start");
+		
+		root.getChildren().addAll(label, btnHowtoPlay, btnStart);
+		
 	}
 	
 	
