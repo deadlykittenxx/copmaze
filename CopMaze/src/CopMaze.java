9819f97584c348573d2d6f77cd09be2f2e986062
deadlykittenxx <96yean@gmail.com>
2023-09-21 15:44:39 +0200

    Initial Scenes: Character, Level

--- CopMaze/src/CopMaze.java
+++ CopMaze/src/CopMaze.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -10,7 +12,15 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class CopMaze extends Application {
-
+	
+	public static final int WIDTH = 640;
+	public static final int HEIGHT = 480;
+	
+	public Scene characterScene;
+	public Scene levelScene;
+	public EventHandler<ActionEvent> btnStartListener;
+	public EventHandler<ActionEvent> btnCharacterListener;
+	
 	public static void main(String[] args){
 		launch(args);
 	}
@@ -20,13 +30,88 @@ public class CopMaze extends Application {
 		
 	
 		VBox root = new VBox();
-		Scene scene = new Scene(root, 700, 500);
+		Scene scene = new Scene(root);
 		root.setAlignment(Pos.CENTER);
 		
+		VBox characterRoot = new VBox();
+		characterScene = new Scene(characterRoot);
+		characterRoot.setAlignment(Pos.CENTER);
+		
+		VBox levelRoot = new VBox();
+		levelScene = new Scene(levelRoot);
+		levelRoot.setAlignment(Pos.CENTER);
+		
+		initListener(primaryStage);
+		
+		
 		initGUI(root);
+		characterGUI(characterRoot);
+		levelGUI(levelRoot);
+		
 		primaryStage.setTitle("Cop Maze"); 
+		primaryStage.setWidth(WIDTH);
+		primaryStage.setHeight(HEIGHT);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	
+	/*
+	 * 	Initialize the listeners
+	 */
+	public void initListener(Stage stage) {
+		btnStartListener = new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				stage.setScene(characterScene);
+			}
+		};
+		
+		btnCharacterListener = new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				/*
+				 * 	Create new Character
+				 */
+				Button btn = (Button) event.getSource();
+				Character player = new Character(btn.getText());
+
+				stage.setScene(levelScene);
+				
+			}
+			
+		};
+		
+	}
+	
+	public void levelGUI(VBox root) {
+		Label label = new Label("Choose Your Level !"); 
+		label.setPadding(new Insets(20, 50, 50, 50));
+		
+		Button btnEasy = new Button("Easy");
+		Button btnHard = new Button("Hard");
+		Button btnSuperHard = new Button("Super Hard");
+		
+		root.getChildren().addAll(label, btnEasy, btnHard, btnSuperHard);
+		
+	}
+	
+	
+	/*
+	 * 	Choose Character
+	 */
+	
+	public void characterGUI(VBox root) {
+		Label label = new Label("Choose Your Character !"); 
+		label.setPadding(new Insets(20, 50, 50, 50));
+		
+		Button btnJhonny = new Button("Johnny");
+		Button btnSarah = new Button("Sarah");
+		
+		root.getChildren().addAll(label, btnJhonny, btnSarah);
+		
+		btnJhonny.setOnAction(btnCharacterListener);
+		btnSarah.setOnAction(btnCharacterListener);
 	}
 	
 	
@@ -40,6 +125,10 @@ public class CopMaze extends Application {
 		
 		Button btnHowtoPlay = new Button("How to Play");
 		Button btnStart = new Button("Start");
+		
+		btnStart.setOnAction(btnStartListener);
+		
+		
 		
 		root.getChildren().addAll(label, btnHowtoPlay, btnStart);
 		
