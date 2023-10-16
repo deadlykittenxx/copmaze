73d801c344e0e51d8e67d1a2ac483dc1e8718e53
Jihyun Park <audrey123012@gmail.com>
2023-10-17 00:38:59 +0200

    Code documentation on Police.java

--- CopMaze/src/Police.java
+++ CopMaze/src/Police.java
@@ -1,17 +1,42 @@
 import java.util.List;
 
+/**
+ * This class represents a policeman in the maze (model class).
+ */
 public class Police {
+	/**
+	 * c is the current coordinates of the policeman
+	 */
 	public Coordinate c;
+	/**
+	 * originC is the origin coordinates of the policeman
+	 */
 	public Coordinate originC;
+	/**
+	 * moveTurn is the number of turns the AI policeman has moved
+	 */
 	public int moveTurn = 0;
+	/**
+	 * turnShift is a random number that allows the AI to behave differently for each policeman
+	 */
 	public int turnShift;
 	
+	/**
+	 * Creates a policeman starting at some coordinates.
+	 * @param c the starting coordinates of the policeman
+	 * @param maxTurnShift the maximum value for the random number turnShift
+	 */
     Police(Coordinate c, int maxTurnShift) {
         this.c = c;
         originC = c;
 		turnShift = (int) (Math.random() * (maxTurnShift+1));
     }
     
+    /**
+     * Moves the policeman by one tile in the maze towards the player
+     * @param maze the maze in which to move
+     * @return the new coordinates of the policeman
+     */
     public Coordinate moveToPlayer(Maze maze) {
 		List<Coordinate> path = maze.shortestPath(c, maze.getCharacter().currentLocation);
 		if (!path.isEmpty()) {
@@ -20,6 +45,11 @@ public class Police {
 		return c;
     }
     
+    /**
+     * Moves the policeman by one tile in the maze towards the origin coordinates of the policeman
+     * @param maze the maze in which to move
+     * @return the new coordinates of the policeman
+     */
     public Coordinate moveToOrigin(Maze maze) {
 		List<Coordinate> path = maze.shortestPath(c, originC);
 		if (!path.isEmpty()) {
@@ -28,6 +58,11 @@ public class Police {
 		return c;
     }
 
+    /**
+     * Moves the policeman by one tile in the maze in a random direction
+     * @param maze the maze in which to move
+     * @return the new coordinates of the policeman
+     */
 	public Coordinate moveRandom(Maze maze) {
 		List<Coordinate> possibleMoves = maze.getValidMoves(c);
 		if (possibleMoves.size() > 0) {
